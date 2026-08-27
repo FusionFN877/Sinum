3042a4b64c0a24df14a19099a97df91b9be10808
FusionFN877 <esporttipiak@gmail.com>
2026-08-27 13:26:05 +0200

    Change API_URL to new IP address

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"https://api.novafn.dev"
+#define API_URL @"http://151.242.147.218:3551"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
